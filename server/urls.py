4c0078d7e8d2d9a763c8dc1459a82abb73924817
yiliwang <lynn.wang@oracle.com>
2019-04-22 23:51:35 +0800

    config url and settings for server

---
+++ server/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"))
+]
