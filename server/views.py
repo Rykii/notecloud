08f5f3113fbdc41c794a32a58c06235a0d7e2564
yiliwang <lynn.wang@oracle.com>
2019-04-29 00:09:17 +0800

    sign in: view & server

--- server/views.py
+++ server/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def signIn(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None and user.is_active:
+			login(request, user)
+			print ('login successfully...')
+			return HttpResponseRedirect('/home')
+		else:
+			error = json.dumps({errorCode: 10000, errorMessage: 'Invalid username or password!'})
+			return JsonResponse(error)
+
